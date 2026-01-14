8670c0a5d8e53ae6363e46396f934c8bc73dfaf4
Ken Graf <kengraf57@gmail.com>
2026-01-14 13:58:38 -0500

    Update and rename website/oidc.js to website/scripts/oidc.js

--- website/scripts/oidc.js
+++ website/scripts/oidc.js
@@ -3,7 +3,7 @@ function checkSessionCookie() {
   const cookies = document.cookie;
   const sessionCookie = cookies.split('; ').find(row => row.startsWith('session='));
 
-  if (!sessionCookie) {
-    window.location.href = "/login.html"; 
-  }
+//  if (!sessionCookie) {
+//    window.location.href = "/login.html"; 
+//  }
 }
