fbdb7b909a484c89ca466cdd89f73ecf3e6ee5aa
Ken Graf <ken@securityxing.com>
2026-01-14 13:43:53 -0500

    login

--- dev/fetch_reduce.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for week in {920..1625}; do
-    echo "Week: $week"
-    curl -s https://theweekinchess.com/zips/twic${week}g.zip -o ${week}.zip
-    unzip -qq -u ${week}.zip
-    python fetch_reduce.py twic${week}.pgn
-    wc first10.pgn
-    rm ${week}.zip twic${week}.pgn
-done
