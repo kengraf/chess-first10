0deebc858ad3125ab7e7a5e0e0507bc8bc4ace3f
Ken Graf <kengraf57@gmail.com>
2026-02-03 12:20:52 -0500

    Update databaseItems.py

--- deploy/lambda/databaseItems.py
+++ deploy/lambda/databaseItems.py
@@ -4,14 +4,14 @@ import os
 
 # Initialize outside the handler for connection re-use
 dynamodb = boto3.resource('dynamodb')
-TABLE_NAME = os.environ.get('TABLE_NAME', 'chess-first10-SessionTable')
+TABLE_NAME = os.environ.get('TABLE_NAME', 'chess-first10')
 table = dynamodb.Table(TABLE_NAME)
 
-def lambda_handler(event, context):
+def handler(event, context):
     print("Received event:", json.dumps(event, indent=2))
     try:
         # 1. Parse the incoming body
-        body = json.loads(event.get('body', '{}'))
+        body = event.get('body', '{}')
         sub = body.get('sub')
         id_token = body.get('idToken')
         new_sessions = body.get('sessions', [])
@@ -55,7 +55,14 @@ def lambda_handler(event, context):
 
 if __name__ == "__main__":
     try:
-        event = { "body": '{ "sub": "123abc", "idToken": { "name": "bob", "email": "bob@bob.com"}, "sessions": [ {"date": "11111", "blue": "10", "green": "20",   "yellow": "30", "red": "40" }],  "missed": ["1.e4 e5", "1.d4 d5"] }'}
-        print( lambda_handler(event, 0) )
+        event = { "body": { "sub": "123abc", "idToken": { "name": "bob", "email": "bob@bob.com"}, "sessions": [ {"date": "11111", "blue": "10", "green": "20",   "yellow": "30", "red": "40" }],  "missed": ["1.e4 e5", "1.d4 d5"] }}
+        print( handler(event, 0) )
     except Exception as e:
         print( str(e) )
+
+"""  CLI example
+curl -X POST https://chess-first10.kengraf.com/v1/databaseItems \
+     -H 'Content-Type: application/json' \
+     -d '{ "body": { "sub": "123abc", "idToken": { "name": "bob", "email": "bob@bob.com"}, "sessions": [ {"date": "11111", "blue": "10", "green": "20",   "yellow": "30", "red": "40" }],  "missed": ["1.e4 e5", "1.d4 d5"] }}'
+     
+"""
